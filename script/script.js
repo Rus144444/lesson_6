3eadc08f1bc4a0227c0b24c614b8b13a05ef1ca4
Ruslan <az144444z@mail.com>
2024-10-30 11:30:33 +0100

    exercise 1

---
+++ script/script.js
@@ -0,0 +1,25 @@
+const categoriesElement = document.querySelector("#categories");
+const itemEl = categoriesElement.querySelectorAll(".item");
+console.log(itemEl.forEach((el, ind) => console.log(el, ind + 1)));
+console.log(itemEl.forEach((el) => console.log(el.textContent)));
+
+// Напиши скрипт который:
+// Посчитает и выведет в консоль количество категорий в ul#categories, то есть
+// элементов li.item.
+// Для каждого элемента li.item в списке ul#categories, найдет и выведет в консоль
+//  текст заголовка элемента (тега <h2>) и количество элементов в категории
+// (всех вложенных в него <li>).
+// Для выполнения этой задачи нужно использовать метод forEach() и свойства
+// навигации по DOM.
+// В результате, в консоли будут выведены такие сообщения.
+
+// Number of categories: 3
+
+// Category: Animals
+// Elements: 4
+
+// Category: Products
+// Elements: 3
+
+// Category: Technologies
+// Elements: 5
